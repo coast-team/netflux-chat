42563c419e56dc73ddb1d49626a02c83e78735db
Bruno SINTEFF <sinteff.bruno@gmail.com>
2016-06-13 14:03:56 +0200

    change mock

--- app/user/mock-users.js
+++ app/user/mock-users.js
@@ -1,10 +1,6 @@
 "use strict";
 var user_1 = require('./model/user');
 exports.USERS = [
-    new user_1.User("0", "0", "Chat App"),
-    new user_1.User("1", "1", "Chat App"),
-    new user_1.User("2", "2", "Chat App"),
-    new user_1.User("3", "3", "Chat App"),
-    new user_1.User("4", "4", "Chat App")
+    new user_1.User("0", "0", "Chat App")
 ];
 //# sourceMappingURL=mock-users.js.map
--- app/user/mock-users.ts
+++ app/user/mock-users.ts
@@ -1,9 +1,5 @@
 import { User } from './model/user';
 
 export var USERS : User[] = [
-  new User("0","0","Chat App"),
-  new User("1","1","Chat App"),
-  new User("2","2","Chat App"),
-  new User("3","3","Chat App"),
-  new User("4","4","Chat App")
+  new User("0","0","Chat App")
 ];
